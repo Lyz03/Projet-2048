376ca520063baaa5ec9becc3e901ae1dfe4c6568
Lyz03 <lizoe@netcourrier.com>
2021-12-16 09:33:45 +0100

    css

---
+++ app.js
@@ -0,0 +1,5 @@
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+const line4 = document.getElementById('line5');
+
