2862dd24b6f0add94d0f13018902e5e56c8a1df1
Lyz03 <lizoe@netcourrier.com>
2021-12-16 12:25:11 +0100

    start js

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-const line1 = document.getElementById('line1');
-const line2 = document.getElementById('line2');
-const line3 = document.getElementById('line3');
-const line4 = document.getElementById('line5');
+const line1 = document.querySelectorAll('#line1 div');
+const line2 = document.querySelectorAll('#line2 div');
+const line3 = document.querySelectorAll('#line3 div');
+const line4 = document.querySelectorAll('#line4 div');
 
+let game = Array.from(line1).concat(Array.from(line2), Array.from(line3), Array.from(line4));
+
+
+const scoreSpan = document.getElementById('score');
+const buttonPlay = document.getElementById('play');
+
+// pick a random case
+function randomCase(g) {
+    const randomCaseIndex = Math.floor(Math.random() * g.length);
+    if (g[randomCaseIndex].innerText === '') {
+        return randomCaseIndex;
+    } else {
+        return randomCase(g);
+    }
+}
+
+// add 2 or 4
+function newNumber() {
+    let randomNumber = Math.round(Math.random() * 100);
+    const randomC = randomCase(game);
+    let div = game[randomC];
+
+    if (randomNumber >= 95) {
+        div.innerText = "4";
+        div.style.backgroundColor = "#dcbbd9";
+    } else {
+        try {
+            div.innerText = "2"
+            div.style.backgroundColor = "#e4bacd"
+        }
+        catch (e) {
+
+        }
+    }
+
+}
+
+buttonPlay.addEventListener("click", () => {
+    newNumber();
+});
